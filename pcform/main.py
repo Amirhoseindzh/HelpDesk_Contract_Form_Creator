c1245ac012f76e577760bb23125d757bccb6cd2e
Amirhossein Dezhabdolahi <11095582-Amirhosseindzh@users.noreply.gitlab.com>
2024-03-22 09:59:17 +0330

    initial commit

---
+++ pcform/main.py
@@ -0,0 +1,26 @@
+
+def main_menu(user_input):
+    print("SELECT A MENUE WITH ENTER A NUMBER:")
+    print(
+    """
+    1. Create a New Form
+    2. Serach Forms
+
+    """
+    )
+    if user_input == 1:
+        create_form()
+
+def create_form():
+    print("Customer firstname:")
+    print("Customer lastname:")
+    print("Dvice Model Name:")
+    print("Device Serial Number:")
+    print("Dvice Problems:")
+    print("Serviceman firstname ,lastname:")
+    print("Descriptions:")
+    
+    
+
+if __name__ == "__main__":
+    pass 
